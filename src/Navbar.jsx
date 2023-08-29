9736cac16f83d65c89ca97c520691632b63b6f5d
Isabelle <loariyafiaba@gmail.com>
2023-08-30 00:15:48 +0100

    Added more styling to the page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <>
       <nav>
-        <img src="https://em-content.zobj.net/source/softbank/145/frog-face_1f438.png" alt="frog emoji" />
+        <img src="https://em-content.zobj.net/source/softbank/145/frog-face_1f438.png" width={100} alt="frog emoji" />
         <h1>Froggie To-Do</h1>
         <small>A user-friendly to-do web app, built with React!</small>
       </nav>
