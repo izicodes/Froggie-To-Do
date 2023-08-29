f8d9334b1f931fcb89701f0b43372e7ed98a7aea
Isabelle <loariyafiaba@gmail.com>
2023-08-29 21:55:45 +0100

    Added the list container + styled it

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import "./style.css";
 
 export default function MainApp() {
   return (
-    <form action="" className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="newItem">Add New Item:</label>
-        <input type="text" name="newItem" id="newItem" />
-      </div>
-      <button className="btn">Add item!</button>
-    </form>
+    <>
+      <form action="" className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="newItem">Add New Item:</label>
+          <input type="text" name="newItem" id="newItem" />
+        </div>
+        <button className="btn">Add item!</button>
+      </form>
+      <h1 className="header">To-Do List!</h1>
+      <ul className="list">
+        <li>
+          <label><input type="checkbox"/>Item 1</label>
+          <button className="btn btn-danger">Delete me!</button>
+        </li>
+      </ul>
+    </>
   );
 }
