2324f986c95c5c337dc0f76c188187060cf6e089
Isabelle <loariyafiaba@gmail.com>
2023-08-29 22:24:25 +0100

    Created the setToDos functions to create the list array

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,44 @@ import "./style.css";
 
 export default function MainApp() {
   const [newItem, setNewItem] = useState("");
+  const [todos, setToDos] = useState([]);
+
+  function handleSubmit(e) {
+    // prevents page from refreshing when clicking the button
+    e.preventDefault();
+
+    setToDos((currentTodo) => {
+      return [
+        // ...currentTodo is used to create a copy of the currentTodo array.
+        // all the existing to-do items in the currentTodo array will be included in the new array being created.
+        ...currentTodo,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
+
   return (
     <>
-      <form action="" className="new-item-form">
+      <form onSubmit={handleSubmit} action="" className="new-item-form">
         <div className="form-row">
           <label htmlFor="newItem">Add New Item:</label>
-          <input value={newItem} type="text" onChange={e => setNewItem(e.target.value)} name="newItem" id="newItem" />
+          <input
+            value={newItem}
+            type="text"
+            onChange={(e) => setNewItem(e.target.value)}
+            name="newItem"
+            id="newItem"
+          />
         </div>
         <button className="btn">Add item!</button>
       </form>
       <h1 className="header">To-Do List!</h1>
       <ul className="list">
         <li>
-          <label><input type="checkbox"/>Item 1</label>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
           <button className="btn btn-danger">Delete me!</button>
         </li>
       </ul>
